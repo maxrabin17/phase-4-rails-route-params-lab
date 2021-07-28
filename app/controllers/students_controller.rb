803fc990b24854de2233f7ee9594ede1e9b03252
maxrabin <max.rabin17@gmail.com>
2021-07-28 15:45:13 -0400

    Finished Bonus

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,16 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
-    render json: students
+    if params[:name]
+      students = Student.all.filter do |student|
+        student.first_name.downcase.include?(params[:name].downcase) || student.last_name.downcase.include?(params[:name].downcase)
+      end
+
+      render json: students
+    else
+      students = Student.all
+      render json: students
+    end
   end
 
   def show
